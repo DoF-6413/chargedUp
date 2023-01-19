676e9e38f39d3dd7b4574d59cd1f62ed1cda45d9
Sawyer <sawyerlee29@gmail.com>
2023-01-18 19:54:08 -0700

    adding botton logging

--- chargedUp/src/main/java/frc/robot/RobotContainer.java
+++ chargedUp/src/main/java/frc/robot/RobotContainer.java
@@ -8,7 +8,13 @@ import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
+
+import javax.swing.plaf.basic.BasicOptionPaneUI.ButtonActionListener;
+
+import edu.wpi.first.wpilibj.XboxController.Button;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
@@ -49,6 +55,8 @@ public class RobotContainer {
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
     m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+
+    m_driverController.a().whileTrue(new RunCommand(() ->SmartDashboard.putString("button pressed", "a")) );
   }
 
   /**
