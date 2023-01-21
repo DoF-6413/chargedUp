8531a634b4dc6cdddf4a0940ffe69d2ac5f63ee5
El1zabethS <Bestlemon555@gmail.com>
2023-01-21 14:50:22 -0700

    Working arm codegit add .! (Not pid tuned)

--- chargedUp/src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ chargedUp/src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -12,6 +12,10 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
