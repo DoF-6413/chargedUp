70cbc5275adfea1487845864f342d0709a2108fb
El1zabethS <BestLemon555@gmail.com>
2023-02-15 20:02:22 -0700

    Removed motor saftey

--- chargedUp/src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
+++ chargedUp/src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -117,7 +117,7 @@ public class DrivetrainSubsystem extends SubsystemBase {
     rightFollower2.follow(rightLead);
 
     diffDrive = new DifferentialDrive(leftLead, rightLead);
-    diffDrive.setSafetyEnabled(true);
+    diffDrive.setSafetyEnabled(false);
 
     m_odometry = new DifferentialDriveOdometry(
         gyro.getRotation2d(), DrivetrainSubsystem.getDistanceLeaftlead(), DrivetrainSubsystem.getDistanceRigthlead());
@@ -229,7 +229,6 @@ public class DrivetrainSubsystem extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    System.out.println("is running periodic");
     updateOdometry();
     if (m_field2d != null
     // && m_odometry.getPoseMeters() != null
