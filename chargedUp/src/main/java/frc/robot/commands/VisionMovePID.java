98fd42e8fb24ab839c45f6028b5239079a7a1bd3
El1zabethS <Bestlemon555@gmail.com>
2023-02-24 18:34:07 -0700

    Vision PID Controls - NEEDS TUNING -

--- chargedUp/src/main/java/frc/robot/commands/VisionMovePID.java
+++ chargedUp/src/main/java/frc/robot/commands/VisionMovePID.java
@@ -8,31 +8,41 @@ import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.PIDCommand;
 import frc.robot.Constants.DrivetrainConstants;
 import frc.robot.subsystems.DrivetrainSubsystem;
+import frc.robot.subsystems.VisionSubsystem;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class VisionMovePID extends PIDCommand {
   /** Creates a new VisionMovePID. */
-  public VisionMovePID(DrivetrainSubsystem drive) {
+  private DrivetrainSubsystem m_drivetrainSubsystem;
+  private VisionSubsystem m_visionSubsystem;
+  public VisionMovePID(DrivetrainSubsystem drive, VisionSubsystem vision) {
     super(
         // The controller that the command will use
         new PIDController(0, 0, 0),
         // This should return the measurement
-        () -> 0,
+        () -> vision.distanceFinder().getX(),
         // This should return the setpoint (can also be a constant)
-        () -> 0,
+        () -> 9,
         // This uses the output
         output -> { drive.setRaw(output/180, DrivetrainConstants.kStopMotors);
           // Use the output here
         });
     // Use addRequirements() here to declare subsystem dependencies.
+    m_drivetrainSubsystem = drive;
+    m_visionSubsystem = vision;
     // Configure additional PID options by calling `getController` here.
+    getController().setTolerance(DrivetrainConstants.kMoveTolerance);
   }
 
+  @Override
+  public void initialize() {
+    m_drivetrainSubsystem.resetPosition();
+  }
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return (getController().atSetpoint() == true);
   }
 }
