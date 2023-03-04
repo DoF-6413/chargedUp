69d8f699a064de31670704fc15765910de7c11e6
El1zabethS <Bestlemon555@gmail.com>
2023-03-04 10:52:13 -0700

    created more efficent method for testing/tuning

--- chargedUp/src/main/java/frc/robot/commands/MovePID.java
+++ chargedUp/src/main/java/frc/robot/commands/MovePID.java
@@ -5,30 +5,34 @@
 package frc.robot.commands;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.PIDCommand;
-import frc.robot.Constants.DrivetrainConstants;
 import frc.robot.subsystems.DrivetrainSubsystem;
-import frc.robot.subsystems.GyroSubsystem;
 
-/** Balances robot when on charging station (Assumes no other robot is on charging station && is already on charging station)  */
-public class gyroMovePID extends PIDCommand {
-  /** Creates a new gyroMovePID. */
-  public gyroMovePID(GyroSubsystem gyro, DrivetrainSubsystem drive) {
+
+/** THIS FILE IS FOR PID TUNING/TESTING. IF MOVING IN AUTO, PREFERABLY USE PATH PLANNER */
+public class MovePID extends PIDCommand {
+  /** Creates a new MovePID. */
+  public MovePID(DrivetrainSubsystem drive, double setpoint) {
     super(
         // The controller that the command will use
-        new PIDController(DrivetrainConstants.kMoveP, DrivetrainConstants.kMoveI, DrivetrainConstants.kMoveD),
+        new PIDController(
+        SmartDashboard.getNumber("kMoveP", 0), 
+        SmartDashboard.getNumber("kMoveI", 0), 
+        SmartDashboard.getNumber("kMoveD", 0)
+        ),
         // This should return the measurement
-        () -> gyro.getPitch(),
+        () -> drive.getPosition(),
         // This should return the setpoint (can also be a constant)
-        () -> 0,
+        () -> setpoint,
         // This uses the output
         output -> { drive.setRaw(output, 0);
           // Use the output here
         });
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(drive, gyro);
+    addRequirements(drive);
     // Configure additional PID options by calling `getController` here.
-    getController().setTolerance(3);
+    getController().setTolerance(1);
   }
 
   // Returns true when the command should end.
