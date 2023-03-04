42619b5034265335bb07969f197ae802ad4ed95c
El1zabethS <Bestlemon555@gmail.com>
2023-03-04 10:39:37 -0700

    WIP Gyro move to charging station, needs PID Tuning

--- chargedUp/src/main/java/frc/robot/commands/gyroMovePID.java
+++ chargedUp/src/main/java/frc/robot/commands/gyroMovePID.java
@@ -4,29 +4,38 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.PIDCommand;
+import frc.robot.Constants.DrivetrainConstants;
+import frc.robot.subsystems.DrivetrainSubsystem;
+import frc.robot.subsystems.GyroSubsystem;
 
-public class gyroMovePID extends CommandBase {
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class gyroMovePID extends PIDCommand {
   /** Creates a new gyroMovePID. */
-  public gyroMovePID() {
+  public gyroMovePID(GyroSubsystem gyro, DrivetrainSubsystem drive) {
+    super(
+        // The controller that the command will use
+        new PIDController(DrivetrainConstants.kMoveP, DrivetrainConstants.kMoveI, DrivetrainConstants.kMoveD),
+        // This should return the measurement
+        () -> gyro.getPitch(),
+        // This should return the setpoint (can also be a constant)
+        () -> 0,
+        // This uses the output
+        output -> { drive.setRaw(output, 0);
+          // Use the output here
+        });
     // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(drive, gyro);
+    // Configure additional PID options by calling `getController` here.
+    getController().setTolerance(3);
   }
-
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {}
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {}
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return getController().atSetpoint();
   }
 }
