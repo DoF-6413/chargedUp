3a5fb115da33cc3c3d6639752de287a9ff2256fd
nancyesquivel10 <nancyesquivel505@gmail.com>
2023-03-07 17:35:14 -0700

    PID tune for telescoper is working

--- chargedUp/src/main/java/frc/robot/Constants.java
+++ chargedUp/src/main/java/frc/robot/Constants.java
@@ -142,7 +142,7 @@ public final class Constants {
     public static final double kArmMaxAcceleration = 100;
     
     // Telescoper PID Values
-    public static final double kTelescoperP = 0.10;
+    public static final double kTelescoperP = 0.04;
     public static final double kTelescoperI = 0;
     public static final double kTelescoperD = 0;
     public static final double kTelescoperTolerance = 0.5;
