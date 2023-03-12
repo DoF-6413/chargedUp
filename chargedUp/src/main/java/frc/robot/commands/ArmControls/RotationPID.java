f56ebe906f3e0567f09cf7091f23296088923476
El1zabethS <Bestlemon555@gmail.com>
2023-03-12 13:04:52 -0700

    Updated for new arm :/

--- chargedUp/src/main/java/frc/robot/commands/ArmControls/RotationPID.java
+++ chargedUp/src/main/java/frc/robot/commands/ArmControls/RotationPID.java
@@ -49,6 +49,7 @@ public class RotationPID extends ProfiledPIDCommand {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
+    // return false;
     return getController().atGoal();
   }
 }
