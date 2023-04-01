e8da23f9754a74e9abb4c903f6c9cd576398450f
nancyesquivel10 <nancyesquivel505@gmail.com>
2023-03-31 18:57:46 -0700

    Geting robot pose!

--- chargedUp/src/main/java/frc/robot/Constants.java
+++ chargedUp/src/main/java/frc/robot/Constants.java
@@ -111,7 +111,7 @@ public final class Constants {
     public static final double[] ksetpoints = new double[] { 0, 1.5, 3, 4 };
 
     public static final Pose3d[] tagPoses = new Pose3d[] {
-    new Pose3d(null),
+    new Pose3d(10,10,10,new Rotation3d(new Quaternion(2.0,2.0,1.0, 1.0))),
     new Pose3d(new Translation3d(15.513558,1.071626,0.462788), new Rotation3d(new Quaternion(0.0, 0.0, 0.0, 1.0))),//id 1
     new Pose3d(new Translation3d(0, 0, 0), new Rotation3d(new Quaternion(0, 0, 0, 0))),//id 2
     new Pose3d(new Translation3d(0, 0, 0), new Rotation3d(new Quaternion(0, 0, 0, 0))),//id 3
