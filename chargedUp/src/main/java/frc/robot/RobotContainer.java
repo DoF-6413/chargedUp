ede91c8f987d3edf34575347eaf20568e39b8755
victor oundjian <vgo89ed@gmail.com>
2023-04-09 16:01:05 -0700

    auto rutines to different grids

--- chargedUp/src/main/java/frc/robot/RobotContainer.java
+++ chargedUp/src/main/java/frc/robot/RobotContainer.java
@@ -23,8 +23,6 @@ import frc.robot.commands.*;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.RamseteController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.trajectory.Trajectory;
