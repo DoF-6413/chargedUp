59495aa458f4473cfe23d68fbb1cdea4a83e6f79
hh3len <helenjling@gmail.com>
2023-09-26 19:51:22 -0700

    continued working on telescoper

--- chargedUp/src/main/java/frc/robot/subsystems/TelescoperPIDSubsystem.java
+++ chargedUp/src/main/java/frc/robot/subsystems/TelescoperPIDSubsystem.java
@@ -4,32 +4,45 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
-
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.State;
 import edu.wpi.first.wpilibj2.command.ProfiledPIDCommand;
 import edu.wpi.first.wpilibj2.command.ProfiledPIDSubsystem;
 import frc.robot.Constants.TelescoperConstants;
 
+
 public class TelescoperPIDSubsystem extends ProfiledPIDSubsystem {
-    private final CANSparkMax telescoperMotorSparkMax;
+    private final TalonFX telescoperMotor;
     private final RelativeEncoder telescoperEncoder;
 
     public TelescoperPIDSubsystem() {
-        super(
-            new ProfiledPIDController (
-                TelescoperConstants.kTelescoperP,
-                TelescoperConstants.kTelescoperI,
-                TelescoperConstants.kTelescoperD, null);
-        )
+        new ProfiledPIDController (
+            TelescoperConstants.kTelescoperP,
+            TelescoperConstants.kTelescoperI,
+            TelescoperConstants.kTelescoperD, 
+            null);
 
-        telescoperMotorSparkMax = new CANSparkMax(telescoperMotorSparkMax, MotorType.kBrushless);
-        telescoperMotorSparkMax.setSmartCurrentLimit(60);
-
+        telescoperMotor = new TalonFX(TelescoperConstants.kTelescoperCANID);
+        StatorCurrentLimitConfiguration currentLimitConfig = new StatorCurrentLimitConfiguration();
     }
 
-    controller.setTolerance(TelescoperConstants.kRotationTolerance);
-    setGoal(TelescoperConstants.kTelescoperOffsetRad);
-}
+    m_controller.setTolerance(TelescoperConstants.kRotationTolerance);
+
+
+	@Override
+	protected void useOutput(double output, State setpoint) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected double getMeasurement() {
+		// TODO Auto-generated method stub
+		return 0;
+	}}
 
